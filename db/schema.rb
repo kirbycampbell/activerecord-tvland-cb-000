fcce6447951e38730269e3d7fd14ca395d0bdfab
Kirby Campbell <jkirbycampbell@gmail.com>
2018-07-11 18:23:47 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 4) do
-
-  create_table "actors", force: :cascade do |t|
-    t.string "name"
-  end
+ActiveRecord::Schema.define(version: 2) do
 
   create_table "networks", force: :cascade do |t|
     t.string "call_letters"
@@ -23,7 +19,6 @@ ActiveRecord::Schema.define(version: 4) do
 
   create_table "shows", force: :cascade do |t|
     t.string "name"
-    t.integer "network_id"
   end
 
 end
