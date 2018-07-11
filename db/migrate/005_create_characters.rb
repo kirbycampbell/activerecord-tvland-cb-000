9fd4984319eac6b33676135a67bc0d3c77aceb3d
Kirby Campbell <jkirbycampbell@gmail.com>
2018-07-11 18:21:43 +0000

    automatically backed up by learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,5 +1,5 @@
 
-class CreateCharacter < ActiveRecord::Migration[4.2]
+class CreateCharacters < ActiveRecord::Migration[4.2]
   def change
     create_table :characters do |t|
       t.string :name
