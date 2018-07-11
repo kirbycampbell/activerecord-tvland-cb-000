c84e19bb48a174997590affd21f133d20f0990cf
Kirby Campbell <jkirbycampbell@gmail.com>
2018-07-11 18:20:53 +0000

    automatically backed up by learn

--- db/migrate/007_add_character_name_and_show_id.rb
+++
@@ -1,7 +0,0 @@
-class AddCharacterNameAndShowId < ActiveRecord::Migration[4.2]
-
-  def change
-    add_column :characters, :name, :string
-    add_column :characters, :show_id, :integer
-  end
-end
