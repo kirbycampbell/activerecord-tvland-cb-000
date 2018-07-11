fb117946b154116b14876c6f2bd94eea7bb9e995
Kirby Campbell <jkirbycampbell@gmail.com>
2018-07-11 19:17:31 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,7 +7,8 @@ class Actor < ActiveRecord::Base
   end
 
   def list_roles
-    var = []
-    var << self.character.name
+    self.characters.map do |character|
+      character.name + " - " character.show.name
+    end
   end
 end
